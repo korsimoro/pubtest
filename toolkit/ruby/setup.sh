8b57582d324a01d0ad5739a03b2f3da8ac27a292
ewelton <eric@korsimoro.com>
2019-04-06 19:44:39 +0700

    test

--- toolkit/ruby/setup.sh
+++ toolkit/ruby/setup.sh
@@ -3,6 +3,14 @@ TOOLKIT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $TOOLKIT_DIR
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 export rvm_path=$TOOLKIT_DIR/.rvm
-rm -rf $rvm-path
+echo rm -rf $rvm_path
+rm -rf $rvm_path
+echo "Installing RVM in $rvm_path"
 curl -sSL https://get.rvm.io | bash -s
 echo "Activate with : . $rvm_path/scripts/rvm"
+
+echo "Activating"
+. $rvm_path/scripts/rvm
+echo "running bundle install to load all the Gemfile bits"
+bundle install
+

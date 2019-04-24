d691bfd20b26c41b57d70df3ccf76bc5fa0a3ed4
Eskimo Eric <eric@iglu.net>
2019-04-24 09:57:31 +0700

    removed ruby

--- toolkit/python/setup.sh
+++ toolkit/python/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TOOLKIT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $TOOLKIT_DIR
-virtualenv -p python3 ./venv
+python3 -m venv ./venv
 . ./venv/bin/activate
 pip install -r requirements.txt
 pip install -e .
--- toolkit/setup.sh
+++ toolkit/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 TOOLKIT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 PYTHON_TOOLKIT_DIR=$TOOLKIT_DIR/python
-RUBY_TOOLKIT_DIR=$TOOLKIT_DIR/ruby
 $PYTHON_TOOLKIT_DIR/setup.sh
-$RUBY_TOOLKIT_DIR/setup.sh
+
+# RUBY_TOOLKIT_DIR=$TOOLKIT_DIR/ruby
+# $RUBY_TOOLKIT_DIR/setup.sh
